ff485b8a5e26a567f65a2634ae908b74d5133e3d
MATSUMOTO-Takashi <t.matsumoto0308@gmail.com>
2014-07-01 12:13:19 +0900

    インストールの簡略化。 NeoBundleのインストールまで自動化。 ただし警告が出るので、この方法でいいのかは引き続き調査する。

--- README.md
+++ README.md
@@ -3,8 +3,6 @@
 ```
 cd 
 
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
-
 git clone https://github.com/MATSUMOTO-Takashi/dotfiles.git
 
 sh dotfiles/dotfiles_link.sh
--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 
+echo y | vim +":q"
