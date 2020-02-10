5610b681625062808c458eb98ad9538146ceedde
TAKEDA-Takashi <takeda.takashi0308@gmail.com>
2020-02-10 18:37:28 +0900

    clone場所に依存しないように修正

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
 
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s $(pwd)/.vimrc ~/.vimrc
+ln -s $(pwd)/.gitconfig ~/.gitconfig
+ln -s $(pwd)/.gitignore_global ~/.gitignore_global
 
 echo y | vim +":q"
